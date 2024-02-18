d17eb75b2f72251cc36ac02aa91f0e7ff01778da
Berouria Meguira <meguiraberouria@gmail.com>
2024-02-18 19:16:34 +0100

    Ajout du bouton deconnexion

--- app/src/main/java/fr/be2/gsb_medicaments/authentification.java
+++ app/src/main/java/fr/be2/gsb_medicaments/authentification.java
@@ -1,35 +1,52 @@
 package fr.be2.gsb_medicaments;
 
+import static javax.net.ssl.SSLEngineResult.Status.OK;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import java.security.SecureRandom;
 
 public class authentification extends AppCompatActivity {
-    private EditText CodeV,myKey;
-    private Button buttonValiderCodeV,btnValiderCle;
+    private EditText editTextCodeV,myKey;
+    private Button buttonValiderCodeV,btnValiderCle,btnDeconnexion;
     LinearLayout layoutCle;
+    private static final String PREF_NAME = "UserPrefs";
+    private static final String SECURETOKEN = "BethElicheva5";
+    private static final String KEY_USER_STATUS = "userStatus";
+    String myRandomKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentification);
-        CodeV=findViewById(R.id.editTextCodeV);
+        editTextCodeV=findViewById(R.id.editTextCodeV);
         myKey=findViewById(R.id.myKey);
         layoutCle =findViewById(R.id.layoutCle);
         layoutCle.setVisibility(View.INVISIBLE);
-
-    }
+          }
     public void AfficheLayout(View v){
+        String secureKey = "votre_secureKey";
         layoutCle.setVisibility(View.VISIBLE);
-        String myRandomKey=genererChaineAleatoire(5);
+        myRandomKey=genererChaineAleatoire(5);
         Log.d( "APPLI","myKey"+myRandomKey );
+        String codeV = editTextCodeV.getText().toString();
+
+        // Vous pouvez maintenant utiliser la méthode sendKeyByEmail
+        // avec le codeV, secureKey, et token comme paramètres
+
+        String token = SECURETOKEN;
+        SendKeyTask sendKeyTask = new SendKeyTask(getApplicationContext());
+        sendKeyTask.execute(codeV, secureKey, token);
 
     }
     private String genererChaineAleatoire(int longueur) {
@@ -45,6 +62,28 @@ public class authentification extends AppCompatActivity {
         }
 
         return chaineAleatoire.toString();
+    }
+    public void Affiche2Layout(View v){
+        String key = myKey.getText().toString().trim();
+        if (key.equals(myRandomKey)) {
+            showToast("Les chaînes A et B sont égales.");
+            setUserStatus("authentification=OK");
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            showToast("Les chaînes A et B ne sont pas égales.");
+            setUserStatus("authentification=KO");
+        }
+    }
+    private void showToast(String montexte){
+        Toast.makeText(this, montexte, Toast.LENGTH_LONG).show();
+    }
+    private void setUserStatus(String status) {
+        SharedPreferences sharedPreferences = getSharedPreferences(PREF_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(KEY_USER_STATUS, status);
+
+        editor.apply();
     }
 
 }
